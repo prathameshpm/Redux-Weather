4dc47862bc9d072c347c8f4a9c9d13a056ecfb4c
prathameshpm <mahamulkarprathamesh@gmail.com>
2017-09-20 12:28:13 +0530

    Action and Middleware fin commit

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '65e6ff1e46603bb87fede8beb6efb821';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},in`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
